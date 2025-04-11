5331c8c34da919abbde6c16cadb4039b73dcae27
Arfaz31 <arfazahamed31@gmail.com>
2025-04-12 00:20:25 +0600

    add services

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,18 @@ import { Application } from 'express';
 import cors from 'cors';
 import globalErrorHandler from './app/middleware/globalErrorHandler';
 import notFoundRoute from './app/middleware/notFoundRoute';
+import cookieParser from 'cookie-parser';
 
 const app: Application = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    credentials: true,
+  }),
+);
+app.options('*', cors());
+app.use(cookieParser());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Hotlancer server');
--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -23,10 +23,12 @@ export const config = {
   },
 
   bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+
   jwt_access_secret: process.env.JWT_ACCESS_SECRET,
   jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
   jwt_access_expire_in: process.env.JWT_ACCESS_EXPIRES_IN,
   jwt_refresh_expire_in: process.env.JWT_REFRESH_EXPIRES_IN,
+
   reset_pass_secret: process.env.RESET_PASS_TOKEN,
   reset_pass_expire_in: process.env.RESET_PASS_EXPIRES_IN,
   reset_pass_link: process.env.RESET_PASS_LINK,
