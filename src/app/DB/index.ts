3656b0d59affe74dd0156a7b2cd4dff47932c934
Arfaz31 <arfazahamed31@gmail.com>
2025-04-15 00:27:44 +0600

    update auth middleware

--- src/app/DB/index.ts
+++ src/app/DB/index.ts
@@ -1,6 +1,7 @@
 import { config } from '../config';
 import { USER_ROLE } from '../module/User/user.constant';
 import { User } from '../module/User/user.model';
+import { Admin } from '../module/Admin/admin.model';
 
 const superAdminData = {
   name: config.SUPERADMIN.NAME,
@@ -10,18 +11,29 @@ const superAdminData = {
   password: config.SUPERADMIN.PASSWORD,
   gender: config.SUPERADMIN.GENDER,
   role: config.SUPERADMIN.ROLE,
-  // isDeleted: config.SUPERADMIN.IS_DELETED,
 };
 
 export const seedingSuperAdmin = async () => {
   try {
-    const isSuperAtcAdminExits = await User.findOne({
+    const isSuperAdminExist = await User.findOne({
       role: USER_ROLE.SUPER_ADMIN,
     });
 
-    if (!isSuperAtcAdminExits) {
-      await User.create(superAdminData);
-      console.log('Super admin is sedding in database successfully');
+    if (!isSuperAdminExist) {
+      const newAdmin = await User.create(superAdminData);
+
+      const adminData = {
+        userId: newAdmin._id,
+        name: superAdminData.name,
+        userName: newAdmin.userName,
+        email: newAdmin.email,
+        contact: newAdmin.contact,
+        gender: superAdminData.gender,
+      };
+
+      await Admin.create(adminData);
+
+      console.log('Super admin is seeded into the database successfully.');
     } else {
       console.log('Super admin already exists. Skipping creation.');
     }
