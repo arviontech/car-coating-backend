5e771af1c54c7437e054fed37f7da13c0d3cbcc9
Arfaz31 <arfazahamed31@gmail.com>
2025-04-14 19:18:10 +0600

    new commit

--- src/app.ts
+++ src/app.ts
@@ -5,17 +5,19 @@ import cors from 'cors';
 import globalErrorHandler from './app/middleware/globalErrorHandler';
 import notFoundRoute from './app/middleware/notFoundRoute';
 import cookieParser from 'cookie-parser';
+import { MiddlewareRoutes } from './app/route';
 
 const app: Application = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: ['http://localhost:3000'],
     credentials: true,
   }),
 );
 app.options('*', cors());
 app.use(cookieParser());
+app.use('/api/v1', MiddlewareRoutes);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Hotlancer server');
